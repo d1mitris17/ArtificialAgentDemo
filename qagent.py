81503e727b1d53cadec8209e629225c3343f5d57
Dimitris Doukas <dd2040@hw.ac.uk>
2025-12-14 17:50:26 +0000

    Feat: Completed training function and get move

--- qagent.py
+++ qagent.py
@@ -1,8 +1,11 @@
 from game import Game
+import random
 
 
 class QAgent:
-    def __init__(self, alpha=0.5, gamma=0.9):
+    def __init__(
+        self, alpha=0.5, gamma=0.9, epsilon=1.0, epsilon_min=0.05, epsilon_decay=0.995
+    ):
         self.game = Game()
         self.states = []
         for i in range(len(self.game.grid)):
@@ -16,4 +19,39 @@ class QAgent:
                 self.qTable[(state, action)] = 0.0
         self.alpha = alpha  # learning rate
         self.gamma = gamma  # discount factor
-        # self.epsilon = 0.1  # exploration rate, for epsilon-greedy policy
+        self.epsilon = epsilon
+        self.epsilon_min = epsilon_min
+        self.epsilon_decay = epsilon_decay
+
+    def train(self, episodes):
+        for episode in range(episodes):
+            self.game.reset()
+            done = False
+            while not done:
+                s = self.game.pos
+                if random.random() < self.epsilon:
+                    action = random.choice(self.actions)
+                else:
+                    maxQ = max(self.qTable[(s, _)] for _ in self.actions)
+                    possibleActions = [
+                        a for a in self.actions if self.qTable[(s, a)] == maxQ
+                    ]
+                    action = random.choice(possibleActions)
+
+                s2, r, done = self.game.step(action)
+
+                if done:
+                    target = r
+                else:
+                    best_next = max(self.qTable[(s2, a2)]
+                                    for a2 in self.actions)
+                    target = r + self.gamma * best_next
+
+                self.qTable[(s, action)] = (1 - self.alpha) * self.qTable[
+                    (s, action)
+                ] + self.alpha * target
+
+    def getMove(self, pos):
+        maxQ = max(self.qTable[(pos, a)] for a in self.actions)
+        moves = [a for a in self.actions if self.qTable[(pos, a)] == maxQ]
+        return random.choice(moves)
